2ac8bf0bac73a526f31e0e861b186ee526c2f00d
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2013-09-04 16:25:19 -0600

    Initial commit of the Ruby on Rails app first_app for the CSCI 3308 class.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6953ca3ef6b967b9b52b0a2df45a71342dec7ac18ee19ad0ebf40433ea81bf77c7b121e8fe2f0238b94377872aae4f0ab71a2b39a168352eeace4e209857a40f'
